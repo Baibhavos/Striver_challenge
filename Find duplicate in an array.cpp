31b2c8eca5bd8f034bb5b7d7fc80ba69e1e3a562
Baibhav Kumar <61260582+Baibhavos@users.noreply.github.com>
2022-06-17 21:04:59 +0530

    Create Find duplicate in an array.cpp

---
+++ Find duplicate in an array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int findDuplicate(vector<int> &arr, int n){
+	// Write your code here.
+    map<int,int> m;
+    
+    for(int i=0;i<n;i++)
+        m[arr[i]]++;
+    
+    for(auto it:m)
+    {
+        if(it.second>1)
+            return it.first;
+    }
+    return -1;
+}
